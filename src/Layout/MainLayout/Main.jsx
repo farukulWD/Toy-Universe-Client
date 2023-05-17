845f8922768901258105be542792c0bc66953441
Farukul Islam <farukgb1999@gmail.com>
2023-05-18 00:53:14 +0600

    make menubar

--- src/Layout/MainLayout/Main.jsx
+++ src/Layout/MainLayout/Main.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Menubar from '../../Component/Menubar/Menubar';
 
 const Main = () => {
     return (
-        <div>
+        <div className='max-w-[1280px] mx-auto'>
+            <Menubar></Menubar>
             <h2>This is main layout</h2>
         </div>
     );
